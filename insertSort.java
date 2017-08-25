7fc48b3b935ad8f474683e11d6511716a1626186
ousiwen <ousiwen@ousiwendeMacBook-Pro.local>
2017-08-25 23:38:56 +0800

    insertSort

---
+++ insertSort.java
@@ -0,0 +1,11 @@
+public void insertSort(int[] nums){
+	for(int i=1;i<nums.length;i++){
+		int key=nums[i];
+		int j=i-1;
+		while(j>=0 && nums[j] > key){
+			nums[j+1]=nums[j];
+			j--;
+		}
+		nums[j+1]=key;
+	}
+}
